5d5853f2d479d956d4bf8a45068bcdd74e8c1b35
tford-dev <terranceford513@gmail.com>
2021-02-02 14:28:47 -0500

    updated pug files

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const pug = require("pug");
 const {projects} = require("./data/data.json");
 const app = express();
 
+//Setup for view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 //reference to data.json
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use('/static', express.static('public/'));
+
 
 app.get("/", (req, res)=>{
     res.render("index.pug", { projects });
@@ -21,11 +24,12 @@ app.get("/about", (req, res)=>{
     res.render("about.pug");
 });
 
-app.get("/projects/:id", (req, res) =>{
-    const projectsId = req.params.id;
-    const project = projects.find( ({id}) => id === +projectsId);
+app.get("/projects/:id", (req, res, next) =>{
+    const projectId = req.params.id;
+    const project = projects.find( ({id}) => id === +projectId);
     if(project){
         res.render("project.pug", { project });
+        next();
     } else {
         res.sendStatus(404);
     }
