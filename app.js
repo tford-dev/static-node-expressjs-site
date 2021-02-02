1a2e19ad67b46d35ae572b622d97f3da6c9aeaf6
tford-dev <terranceford513@gmail.com>
2021-02-02 12:52:25 -0500

    updated directories

--- app.js
+++ app.js
@@ -7,12 +7,28 @@ const {projects} = require("./data/data.json");
 const app = express();
 
 //reference to data.json
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get("/", (req, res)=>{
     res.render("index.pug", { projects });
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about.pug");
+});
+
+app.get("/projects/:id", (req, res) =>{
+    const projectsId = req.params.id;
+    const project = projects.find( ({id}) => id === +projectsId);
+    if(project){
+        res.render("project.pug", { project });
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.listen(3000, ()=>{
