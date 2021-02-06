13d3357f10c65bedcef773c54349ecb51cec47a4
tford-dev <terranceford513@gmail.com>
2021-02-05 22:46:46 -0500

    Added comments to app.js

--- app.js
+++ app.js
@@ -9,32 +9,32 @@ const morgan = require("morgan");
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//reference to data.json
-app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/static', express.static('public/'));
 app.use(morgan('dev'));
 
-
+//route for home page
 app.get("/", (req, res)=>{
     res.render("index.pug", { projects });
 });
 
+//route for about page, learn more button activates opens this
 app.get("/about", (req, res)=>{
     res.render("about.pug");
 });
 
+//route for project pages
 app.get("/projects/:id", (req, res, next) =>{
     const projectId = req.params.id;
     const project = projects.find( ({id}) => id === +projectId);
     if(project){
         res.render("project.pug", { project });
-        next();
     } else {
         res.sendStatus(404);
     }
 });
 
+//Simple listener to open not-found.pug when a route does not exist
 app.use((req, res, next) => {
     console.log("Error has occured, page not found.")
     res.status(404).render('not-found');
